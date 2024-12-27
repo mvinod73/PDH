101dad4e964a91c9c415d7525030e961eb005dba
Vishwajeet Kumar <vishwajeet.kumar@slkgroup.com>
2024-12-27 10:07:32 +0530

    TestWidget

--- scripts/Main.js
+++ scripts/Main.js
@@ -95,7 +95,7 @@ define("PDH/scripts/Main", [
 						onComplete: function (finalres, headerResponse) {
 							alert("csrfTokenValue data "+csrfTokenValue);
 							alert("response 11data "+finalres.data);
-							/* var fetchedData = finalres.data;
+							var fetchedData = finalres.data;
 							alert("Fetched Data: " + JSON.stringify(fetchedData)); */
 						}
 						});		
